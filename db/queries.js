b06843d7b9544143568a9b3d535f1529d5384618
KvJellyBean <kevinnatan91@gmail.com>
2024-09-23 06:12:11 +0700

    feat: add queries for the database

---
+++ db/queries.js
@@ -0,0 +1,33 @@
+const pool = require("./pool");
+
+async function getMessages() {
+  const query = "SELECT * FROM messages ORDER BY added";
+  const { rows } = await pool
+    .query(query)
+    .catch((err) => console.error("Error executing query", err.stack));
+  return rows;
+}
+
+async function getUserMessage(user) {
+  const query = "SELECT * FROM messages WHERE userMsg = $1";
+  const { rows } = await pool
+    .query(query, [user])
+    .catch((err) => console.error("Error executing query", err.stack));
+  return rows;
+}
+
+async function getUsers() {
+  const query = "SELECT DISTINCT userMsg FROM messages";
+  const { rows } = await pool
+    .query(query)
+    .catch((err) => console.error("Error executing query", err.stack));
+  return rows;
+}
+
+async function addMessage(user, text) {
+  await pool
+    .query("INSERT INTO messages(userMsg, text) VALUES($1, $2)", [user, text])
+    .catch((err) => console.error("Error executing query", err.stack));
+}
+
+module.exports = { getMessages, getUserMessage, getUsers, addMessage };
